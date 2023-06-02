bb984b0335b0afa5a0fe2a6d281f45a482440874
EC2 Default User <ec2-user@ip-172-31-3-148.us-east-2.compute.internal>
2023-06-02 19:20:21 +0000

    New Jenkins changes

--- jtgec2_instance.tf
+++ jtgec2_instance.tf
@@ -1,13 +1,31 @@
-provider "aws" {
-  region = "us-east-2"  # Replace with your desired region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
 }
 
-resource "aws_instance" "JenkinsAssignment" {
+terraform{
+        required_version = ">=0.13"
+}
+
+provider "aws" {
+        region = "us-east-2"
+        access_key = "AKIAU4VI4Z3JBNSKOYOP"
+        secret_key = "G3VMPu08U4B+YLKm1ySNMRBMGquSaJnLW7nwBOo9"
+}
+
+resource "aws_instance" "ec2_assignment_terraform" {
   ami           = "ami-05842f1afbf311a43"  # Replace with your desired AMI ID
   instance_type = "t2.micro"                # Replace with your desired instance type
 
   tags = {
-    Name = "AssignmentInstance"
+    Name = "JenkinsTerraformAssignment"
   }
+
+output "public_ip" {
+  value = aws_instance.ec2_assignment_terraform.public_ip
 }
 
