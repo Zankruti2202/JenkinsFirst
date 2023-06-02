ae6384fa1d5dc2f9a3bbb2f852dcc66843eb025d
EC2 Default User <ec2-user@ip-172-31-3-148.us-east-2.compute.internal>
2023-06-02 19:56:21 +0000

    Few new change

--- jtgec2_instance.tf
+++ jtgec2_instance.tf
@@ -13,12 +13,9 @@ terraform{
 
 provider "aws" {
         region = "us-east-2"
-        access_key = "AKIAU4VI4Z3JBNSKOYOP"
-        secret_key = "G3VMPu08U4B+YLKm1ySNMRBMGquSaJnLW7nwBOo9"
 }
 
 resource "aws_instance" "ec2_assignment_terraform" {
-  ami           = "ami-05842f1afbf311a43"  # Replace with your desired AMI ID
   instance_type = "t2.micro"                # Replace with your desired instance type
 
   tags = {
